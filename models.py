4dfba751d06674704774ea15d875fb48b7c0b84a
Usama Fayyaz <44376365+UsamaFayyaz6@users.noreply.github.com>
2023-10-05 23:47:18 +0500

    Sqlite Database and Todo Item Model created

--- models.py
+++ models.py
@@ -1,42 +1,25 @@
 '''
-Todo APP
+Todo Model APP
 '''
 
-from datetime import datetime
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
+db = SQLAlchemy()
+from datetime import datetime
 
 class TodoItem(db.Model):
-    """
-    Todo Item Model
-    """
     id = db.Column(db.Integer, primary_key=True)
     todo = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text)
-    created_on = db.Column(db.DateTime, default=datetime.now)
+    created_on=db.Column(db.DateTime, default=datetime.now)
 
     def __repr__(self):
         return f'Todo: {self.todo}'
-
+    
     def as_dict(self):
-        """
-        Model to dictionatry
-        """
         return {
             'id': self.id,
             'todo': self.todo,
             'description': self.description,
             'created_on': self.created_on
         }
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
